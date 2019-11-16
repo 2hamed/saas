4c156d1dc6e652d287a256f5bc5a2f8431a3c661
Hamed Momeni <hamed.ma7@gmail.com>
2019-11-16 13:46:44 +0330

    added some comments

--- filestore/filestore.go
+++ filestore/filestore.go
@@ -1,9 +1,11 @@
 package filestore
 
+// FileStore is the interface to store files on an abstract block storage
 type FileStore interface {
 	Store(path string) (string, error)
 }
 
+// NewFileStore returns a concrete implementation of FileStore
 func NewFileStore() (FileStore, error) {
 	return createMinio()
 }
