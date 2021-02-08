ff9f015639165ad7c78c1f29630c93a9e0e279d0
Hamed Momeni <hamed.ma7@gmail.com>
2021-02-09 00:00:01 +0100

    added cloud storage

--- capture/main.go
+++ capture/main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"log"
 	"net"
+	"os"
 
+	"cloud.google.com/go/storage"
 	pb "github.com/2hamed/saas/protobuf"
+	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 )
 
@@ -13,19 +16,39 @@ const port = ":50005"
 
 type server struct {
 	pb.CaptureServer
+
+	capture Capture
 }
 
 func (s *server) Capture(ctx context.Context, in *pb.CaptureRequest) (*pb.CaptureResponse, error) {
+	// s.capture.Save(in.Url)
 	return nil, nil
 }
 
 func main() {
+
+	serviceAccountFile := os.Getenv("GCP_SERVICE_ACCOUNT_FILE_PATH")
+	stoageClient, err := storage.NewClient(context.Background(), option.WithServiceAccountFile(serviceAccountFile))
+	if err != nil {
+		panic(err)
+	}
+
+	bucketName := os.Getenv("GCP_STORAGE_BUCKET_NAME")
+	capture, err := NewCapture(stoageClient, bucketName)
+	if err != nil {
+		panic(err)
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
-	pb.RegisterCaptureServer(s, &server{})
+	pb.RegisterCaptureServer(s, &server{
+		capture: capture,
+	})
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
